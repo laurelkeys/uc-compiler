59b7e04856508b74f12af79c98c75bf9a10391f1
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-03-12 18:35:47 -0300

    Pass on all DFA tests

--- project-1/test_DFAs.py
+++ project-1/test_DFAs.py
@@ -20,7 +20,7 @@ class DFA:
         self.current_state = self.transition_function[(self.current_state, input_value)]
 
     def in_accept_state(self):
-        return self.current_state in accept_states
+        return self.current_state in self.accept_states
 
     def go_to_initial_state(self):
         self.current_state = self.start_state
@@ -35,30 +35,69 @@ class DFA:
 ## misc ###################################################
 ###########################################################
 
-states = { 0, 1, 2, 3 }
-alphabet = { 'a', 'b', 'c', 'd' }
-
-start_state = 0
-accept_states = { 2, 3 }
-
 tf = dict()
 tf[(0, 'a')] = 1; tf[(0, 'b')] = 2; tf[(0, 'c')] = 3; tf[(0, 'd')] = 0
 tf[(1, 'a')] = 1; tf[(1, 'b')] = 2; tf[(1, 'c')] = 3; tf[(1, 'd')] = 0
 tf[(2, 'a')] = 1; tf[(2, 'b')] = 2; tf[(2, 'c')] = 3; tf[(2, 'd')] = 0
 tf[(3, 'a')] = 1; tf[(3, 'b')] = 2; tf[(3, 'c')] = 3; tf[(3, 'd')] = 0
-d = DFA(states, alphabet, tf, start_state, accept_states)
+
+d = DFA(
+        states={0, 1, 2, 3},
+        alphabet={'a', 'b', 'c', 'd'},
+        transition_function=tf,
+        start_state=0,
+        accept_states={2, 3}
+    )
 
 # All strings over {a, b, c} that contain an odd number of b's
 tf = dict()
-odd_b = DFA(states, alphabet, tf, start_state, accept_states)
+tf[(0, 'a')] = 0; tf[(0, 'b')] = 1; tf[(0, 'c')] = 0
+tf[(1, 'a')] = 1; tf[(1, 'b')] = 0; tf[(1, 'c')] = 1
+
+odd_b = DFA(
+        states={0, 1},
+        alphabet={'a', 'b', 'c'},
+        transition_function=tf,
+        start_state=0,
+        accept_states={1}
+    )
 
 # All strings over {a, b, c} that contain an even number of a's and an odd number of b's
 tf = dict()
-even_a_odd_b = DFA(states, alphabet, tf, start_state, accept_states)
+tf[(0, 'a')] = 2; tf[(0, 'b')] = 1; tf[(0, 'c')] = 0 # state 0: even a's and even b's
+tf[(1, 'a')] = 3; tf[(1, 'b')] = 0; tf[(1, 'c')] = 1 # state 1: even a's and  odd b's
+tf[(2, 'a')] = 0; tf[(2, 'b')] = 3; tf[(2, 'c')] = 2 # state 2:  odd a's and even b's
+tf[(3, 'a')] = 1; tf[(3, 'b')] = 2; tf[(3, 'c')] = 3 # state 3:  odd a's and  odd b's
+
+even_a_odd_b = DFA(
+        states={0, 1, 2, 3},
+        alphabet={'a', 'b', 'c'},
+        transition_function=tf,
+        start_state=0,
+        accept_states={1}
+    )
 
 # All strings over {a, b, c, d} of length at least 2 whose second symbol does not appear elsewhere in the string
 tf = dict()
-len_2_unique_second = DFA(states, alphabet, tf, start_state, accept_states)
+tf[(0, 'a')] = 1; tf[(0, 'b')] = 2; tf[(0, 'c')] = 3; tf[(0, 'd')] = 4
+
+tf[(1, 'b')] = 6; tf[(1, 'c')] = 7; tf[(1, 'd')] = 8
+tf[(2, 'a')] = 5; tf[(2, 'c')] = 7; tf[(2, 'd')] = 8
+tf[(3, 'a')] = 5; tf[(3, 'b')] = 6; tf[(3, 'd')] = 8
+tf[(4, 'a')] = 5; tf[(4, 'b')] = 6; tf[(4, 'c')] = 7
+
+tf[(5, 'b')] = 5; tf[(5, 'c')] = 5; tf[(5, 'd')] = 5
+tf[(6, 'a')] = 6; tf[(6, 'c')] = 6; tf[(6, 'd')] = 6
+tf[(7, 'a')] = 7; tf[(7, 'b')] = 7; tf[(7, 'd')] = 7
+tf[(8, 'a')] = 8; tf[(8, 'b')] = 8; tf[(8, 'c')] = 8
+
+len_2_unique_second = DFA(
+        states={0, 1, 2, 3, 4, 5, 6, 7, 8},
+        alphabet={'a', 'b', 'c', 'd'},
+        transition_function=tf,
+        start_state=0,
+        accept_states={5, 6, 7, 8}
+    )
 
 ###########################################################
 ## tests ##################################################
@@ -70,37 +109,34 @@ def test_dfa():
     assert not d.run_with_input_list("abcdabcdabcd")
 
 def test_odd_b():
-    assert not d.run_with_input_list("")
-    assert d.run_with_input_list("b")
-    assert d.run_with_input_list("bbb")
-    assert d.run_with_input_list("abc")
-    assert d.run_with_input_list("abbcab")
-    assert d.run_with_input_list("abcabcabc")
-    assert not d.run_with_input_list("d")
-    assert not d.run_with_input_list("db")
-    assert not d.run_with_input_list("bb")
-    assert not d.run_with_input_list("bbbab")
-    assert not d.run_with_input_list("abcabc")
+    assert not odd_b.run_with_input_list("")
+    assert odd_b.run_with_input_list("b")
+    assert odd_b.run_with_input_list("bbb")
+    assert odd_b.run_with_input_list("abc")
+    assert odd_b.run_with_input_list("abbcab")
+    assert odd_b.run_with_input_list("abcabcabc")
+    assert not odd_b.run_with_input_list("bb")
+    assert not odd_b.run_with_input_list("bbbab")
+    assert not odd_b.run_with_input_list("abcabc")
 
 def test_even_a_odd_b():
-    assert not d.run_with_input_list("")
-    assert d.run_with_input_list("b")
-    assert d.run_with_input_list("bbb")
-    assert d.run_with_input_list("aba")
-    assert d.run_with_input_list("abca")
-    assert d.run_with_input_list("abbcab")
-    assert not d.run_with_input_list("d")
-    assert not d.run_with_input_list("aa")
-    assert not d.run_with_input_list("abc")
-    assert not d.run_with_input_list("abcabcabc")
+    assert not even_a_odd_b.run_with_input_list("")
+    assert even_a_odd_b.run_with_input_list("b")
+    assert even_a_odd_b.run_with_input_list("bbb")
+    assert even_a_odd_b.run_with_input_list("aba")
+    assert even_a_odd_b.run_with_input_list("abca")
+    assert even_a_odd_b.run_with_input_list("abbcab")
+    assert not even_a_odd_b.run_with_input_list("aa")
+    assert not even_a_odd_b.run_with_input_list("abc")
+    assert not even_a_odd_b.run_with_input_list("abcabcabc")
 
 def test_len_2_unique_second():
-    assert not d.run_with_input_list("")
-    assert d.run_with_input_list("bdabc")
-    assert d.run_with_input_list("acbab")
-    assert d.run_with_input_list("bacbd")
-    assert d.run_with_input_list("abcdc")
-    assert not d.run_with_input_list("aa")
-    assert not d.run_with_input_list("dd")
-    assert not d.run_with_input_list("bcabc")
-    assert not d.run_with_input_list("abcbc")
+    assert not len_2_unique_second.run_with_input_list("")
+    assert len_2_unique_second.run_with_input_list("bdabc")
+    assert len_2_unique_second.run_with_input_list("acbab")
+    assert len_2_unique_second.run_with_input_list("bacbd")
+    assert len_2_unique_second.run_with_input_list("abcdc")
+    assert not len_2_unique_second.run_with_input_list("aa")
+    assert not len_2_unique_second.run_with_input_list("dd")
+    assert not len_2_unique_second.run_with_input_list("bcabc")
+    assert not len_2_unique_second.run_with_input_list("abcbc")
