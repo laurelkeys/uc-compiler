5ffc70a8bd9fa28e2f74a76513a50604eac6b8e4
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-07-05 14:58:03 -0300

    Set up to start uC + LLVM integration

---
+++ src/uC_eval.py
@@ -0,0 +1,67 @@
+from ctypes import CFUNCTYPE, c_double
+
+import llvmlite.binding as llvm
+
+from uC_LLVMIR import LLVMCodeGenerator, UCLLVM
+from uC_parser import UCParser
+
+
+class CodeEvaluator:
+    def __init__(self) -> None:
+        llvm.initialize()
+        llvm.initialize_native_target()
+        llvm.initialize_native_asmprinter()
+
+        self.target = llvm.Target.from_default_triple()
+        self.reset()
+
+    def reset(self):
+        self.parser = UCParser()
+        self.generator = LLVMCodeGenerator()
+        # TODO add built-ins
+
+    def eval(self, uC_code: str, optimize: bool = True):
+        ast = self.parser.parse(uC_code)
+        llvm_code = self.generator.compile_llvm(ast)
+
+        llvm_module = llvm.parse_assembly(llvmir=str(llvm_code))
+        llvm_module.verify()  # NOTE dump .ll unoptimized code here
+
+        if optimize:
+            pmb = llvm.create_pass_manager_builder()
+            pm = llvm.create_module_pass_manager()
+
+            # ref.: https://clang.llvm.org/docs/CommandGuide/clang.html#code-generation-options
+            pmb.opt_level  = 2  # 0 = -O0,  1 = -O1, 2 = -O2, 3 = -O3
+            pmb.size_level = 0  # 0 = none, 1 = -Os, 2 = -Oz
+
+            # ref.: http://llvm.org/docs/Passes.html https://stackoverflow.com/a/15548189
+            # pm.add_constant_merge_pass()          # -constmerge
+            # pm.add_dead_arg_elimination_pass()    # -deadargelim
+            # pm.add_function_attrs_pass()          # -functionattrs
+            # pm.add_global_dce_pass()              # -globaldce
+            # pm.add_global_optimizer_pass()        # -globalopt
+            # pm.add_ipsccp_pass()                  # -ipsccp
+            # pm.add_dead_code_elimination_pass()   # -dce
+            # pm.add_cfg_simplification_pass()      # -simplifycfg
+            # pm.add_gvn_pass()                     # -gvn
+            # pm.add_instruction_combining_pass()   # -instcombine
+            # pm.add_licm_pass()                    # -licm
+            # pm.add_sccp_pass()                    # -sccp
+            # pm.add_sroa_pass()                    # -sroa
+
+            pmb.populate(pm)
+            pm.run(llvm_module)
+
+            llvm_module.verify()  # NOTE dump .ll optimized code here
+
+        target_machine = self.target.create_target_machine()
+        with llvm.create_mcjit_compiler(llvm_module, target_machine) as execution_engine:
+            execution_engine.finalize_object()  # NOTE dump .asm machine code here
+
+            fn = llvm_module.get_function(name="main")
+            fn_return_type = UCLLVM.Type.Void  # FIXME get the return type of main
+
+            fn_ptr = CFUNCTYPE(fn_return_type)(execution_engine.get_pointer_to_function(fn))
+            
+            return fn_ptr()  # FIXME args (?)
---
+++ src/uC_lexer.py
@@ -0,0 +1,235 @@
+import ply.lex as lex
+
+# NOTE For simple tokens, the regular expression can be specified as strings,
+#      with the name following 't_' matching exactly one of the names supplied in `tokens`.
+#      When a function is used, the regular expression rule is specified in the function documentation string.
+#      The function takes a single argument of type LexToken, with attributes t.type, t.value, t.lineno, and t.lexpos.
+#      The @TOKEN or @Token decorators can also be used for more complex regular expression rules, defined as variables.
+
+# NOTE Patterns are compiled using the re.VERBOSE flag which can be used to help readability.
+#      However, be aware that unescaped whitespace is ignored and comments are allowed in this mode.
+#      If your pattern involves whitespace, make sure you use \s. If you need to match the # character, use [#].
+
+# NOTE When building the master regular expression, rules are added in the following order:
+#      1. All tokens defined by functions are added in the same order as they appear in the lexer file.
+#      2. Tokens defined by strings are added next, by sorting them in order of decreasing regular expression length
+#         (longer expressions are added first).
+
+# NOTE expected/reserved variables:
+#       - `tokens`, a list that defines all possible token names that can be produced by the lexer
+#       - `t_ignore`, special rule reserved for characters that should be completely ignored in the input stream
+#                     obs.: you can include the prefix "ignore_" in a token declaration to force the token to be ignored
+#       - `literals`, list of literal characters, i.e. single characters that are returned "as is" when encountered by the lexer
+#                     obs.: literals are checked after all of the defined regular expression rules
+#       - `t_error()`, function used to handle lexing errors that occur when illegal characters are detected
+#       - `t_eof()`, function used to handle an end-of-file (EOF) condition in the input
+
+# NOTE To build the lexer, the function lex.lex() is used.
+#      This function uses Python reflection to read the regular expression rules out of the calling context and build the lexer.
+#      Once the lexer has been built, two methods can be used to control the lexer:
+#       - lexer.input(data), Reset the lexer and store a new input string.
+#       - lexer.token(), Return the next token (LexToken instance on success or None if the end of the input text is reached).
+
+###########################################################
+## uC Lexer ###############################################
+###########################################################
+
+class UCLexer:
+    ''' A lexer for the uC programming language.\n
+        After building it, set the input text with `input()`, and call `token()` to get new tokens.
+    '''
+
+    def __init__(self, error_func, cast_numbers=True):
+        ''' Creates a new Lexer.\n
+            `error_func` will be called in case of an error during lexing, with an error message, line and column as arguments.
+        '''
+        self.filename = ''
+        self.error_func = error_func
+        self.last_token = None # last token returned from self.token()
+        self.cast_numbers = cast_numbers # casts values of float and int tokens
+
+    def build(self, **kwargs):
+        ''' Builds the lexer. Must be called after the lexer object is created.\n
+            This method exists separately because the PLY manual warns against calling `lex.lex` inside `__init__`.
+        '''
+        self.lexer = lex.lex(object=self, **kwargs)
+
+    def input(self, text):
+        self.lexer.input(text)
+
+    def token(self):
+        self.last_token = self.lexer.token()
+        return self.last_token
+
+    # Internal auxiliary methods
+    def _find_tok_column(self, token):
+        ''' Find the column of the token in its line. '''
+        last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
+        return token.lexpos - last_cr
+
+    def _make_tok_location(self, token):
+        ''' Returns the token's location as a tuple `(line, column)`. '''
+        return token.lineno, self._find_tok_column(token)
+
+    def _reset_lineno(self):
+        ''' Resets the internal line number counter of the lexer to 1. '''
+        self.lexer.lineno = 1
+
+    def _error(self, msg, token):
+        line, column = self._make_tok_location(token)
+        self.error_func(msg, line, column)
+        self.lexer.skip(1)
+
+    # Scanner (used only for testing)
+    def scan(self, data, print_tokens=True):
+        tokens = []
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            tokens.append(tok)
+            if print_tokens:
+                print(tok)
+        return tokens
+
+    # Reserved keywords
+    keywords = (
+        'IF', 'ELSE',
+        'FOR', 'WHILE',
+        'BREAK', 'RETURN',
+        'ASSERT', 'PRINT', 'READ',
+        'VOID', 'CHAR', 'INT', 'FLOAT', # type specifiers
+    )
+
+    keyword_map = {}
+    for keyword in keywords:
+        keyword_map[keyword.lower()] = keyword
+
+    # Token list
+    tokens = (
+        # identifiers
+        'ID',
+
+        # constants
+        'INT_CONST', 'FLOAT_CONST',
+        'CHAR_CONST', 'STRING_LITERAL',
+
+        # braces, brackets and parenthesis
+        'LBRACE', 'RBRACE',
+        'LBRACKET', 'RBRACKET',
+        'LPAREN', 'RPAREN',
+
+        # comma and semicolon
+        'COMMA',
+        'SEMI',
+
+        # assignment operators: =, *=, /=, %=, +=, -=
+        'EQUALS',
+        'TIMESEQUALS', 'DIVEQUALS',
+        'MODEQUALS',
+        'PLUSEQUALS', 'MINUSEQUALS',
+
+        # binary operators: *, /, %, +, -, <, <=, >, >=, ==, !=, &&, ||
+        'TIMES', 'DIV',
+        'MOD',
+        'PLUS', 'MINUS',
+        'LT', 'LEQ', 'GT', 'GEQ', 'EQ', 'NEQ',
+        'AND', 'OR',
+
+        # unary operators: ++, --, +, -, &, *, !
+        'PLUSPLUS', 'MINUSMINUS',
+        'ADDRESS',
+        'NOT',
+
+        # reserved keywords
+    ) + keywords
+
+    # Completely ignored characters
+    t_ignore = ' \t'
+
+    # Newlines
+    def t_NEWLINE(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count('\n')
+
+    # Delimiters
+    t_LBRACE = r'\{'
+    t_RBRACE = r'\}'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+
+    t_COMMA = r','
+    t_SEMI = r';'
+
+    # Assignment operators
+    t_EQUALS = r'='
+    t_TIMESEQUALS = r'\*='
+    t_DIVEQUALS = r'/='
+    t_MODEQUALS = r'%='
+    t_PLUSEQUALS = r'\+='
+    t_MINUSEQUALS = r'-='
+
+    # Binary operators
+    t_TIMES = r'\*'
+    t_DIV = r'/'
+    t_MOD = r'%'
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+
+    t_LT = r'<'
+    t_LEQ = r'<='
+    t_GT = r'>'
+    t_GEQ = r'>='
+    t_EQ = r'=='
+    t_NEQ = r'!='
+
+    t_AND = r'\&\&'
+    t_OR = r'\|\|'
+
+    # Unary operators
+    t_PLUSPLUS = r'\+\+'
+    t_MINUSMINUS = r'--'
+
+    t_ADDRESS = r'\&'
+
+    t_NOT = r'!'
+
+    # Constants
+    t_CHAR_CONST = r'\'([^\\\n]|(\\.))*?\''
+
+    t_STRING_LITERAL = r'\"([^\\\n]|(\\.))*?\"'
+
+    def t_FLOAT_CONST(self, t): # NOTE keep this before t_INT_CONST
+        r'((0|([1-9][0-9]*))\.[0-9]*)|((|0|([1-9][0-9]*))\.[0-9]+)'
+        if self.cast_numbers:
+            t.value = float(t.value)
+        return t
+
+    def t_INT_CONST(self, t):
+        r'0|([1-9][0-9]*)'
+        if self.cast_numbers:
+            t.value = int(t.value)
+        return t
+
+    # Identifiers and reserved words
+    def t_ID(self, t):
+        r'[a-zA-Z_][a-zA-Z0-9_]*'
+        t.type = self.keyword_map.get(t.value, 'ID')
+        return t
+
+    # Comments
+    def t_C_COMMENT(self, t):
+        r'/\*(.|\n)*?\*/'
+        t.lexer.lineno += t.value.count('\n')
+
+    def t_CPP_COMMENT(self, t):
+        r'//.*?(\n|$)'
+        t.lexer.lineno += 1
+
+    # Error
+    def t_error(self, t):
+        msg = "Illegal character '%s'" % repr(t.value[0])
+        self._error(msg, t)
